7b1e64fbd81f5ef7b86989afb7f712a4489b952b
MintSoup <aregak2005@gmail.com>
2020-03-30 17:55:23 +0400

    good code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Slider from './components/timeline/slider/slider'
 
 function App() {
-    return ( <
-        Slider / >
+    return (
+        <Slider values={[1620, 1920, 1324, 1236, 6434, 7634, 5212, 5235, 5123, 1231, 5432, 5123, 7453]} />
     );
 }
 
--- src/components/timeline/slider/slider.js
+++ src/components/timeline/slider/slider.js
@@ -7,11 +7,10 @@ class Slider extends React.Component {
         this.state = {
             position: 0,
             renderPosition: "0%",
-            coords: { x: 0, y: 0 }
+            coords: { x: 0, y: 0 },
+            vlength: props.values.length - 1
         };
-        this.sliderDragStart = this.sliderDragStart.bind(this);
-        this.sliderDragEnd = this.sliderDragEnd.bind(this);
-        this.sliderDrag = this.sliderDrag.bind(this);
+
         this.selector = React.createRef();
     }
 
@@ -23,46 +22,49 @@ class Slider extends React.Component {
         this.updatePosition(0);
     }
 
-    sliderDrag(e) {
+    sliderDrag = e => {
         let s = this.state;
         s.coords.x = e.screenX;
         s.coords.y = e.screenY;
         this.setState(s);
-        let output = s.coords.x;
-        if (output > this.offset + this.len) output = this.offset + this.len;
-        if (output < this.offset) output = this.offset;
-        this.updatePosition(((output - this.offset) / this.len) * 10.4 - 0.3);
+        let coords = s.coords.x;
+        if (coords > this.offset + this.len) coords = this.offset + this.len;
+        if (coords < this.offset) coords = this.offset;
+        let output = (((coords - this.offset) / this.len) * (1/0.955) - 0.03);
+        this.updatePosition(output * this.state.vlength);
     }
 
-    sliderDragStart(e) {
+    sliderDragStart = (e) => {
         document.addEventListener("mouseup", this.sliderDragEnd);
         document.addEventListener("mousemove", this.sliderDrag);
     }
 
-    sliderDragEnd() {
+    sliderDragEnd = () => {
         document.removeEventListener("mouseup", this.sliderDragEnd);
         document.removeEventListener("mousemove", this.sliderDrag);
 
         this.updatePosition(Math.round(this.state.position));
     }
 
-    updatePosition(n) {
+    updatePosition = n => {
         let outputState = this.state;
         outputState.position = n;
-        outputState.renderPosition = n * 9.65 + 0.7 + "%";
+        let outputPercent = (n / this.state.vlength) * 95.5 + 2;
+        outputState.renderPosition = outputPercent + "%";
         this.setState(outputState);
+    }
+
+    stringifyDates = () => {
+        let out = "";
+        this.props.values.forEach(value => out += (value + " "));
+        return out;
     }
 
     render() {
         return (
             <div
                 ref={this.selector}
-                style={{
-                    width: "40%",
-                    margin: "auto",
-                    marginTop: "20%",
-                    padding: "0"
-                }}
+                className="slider"
             >
                 <div className="slider-bar">
                     <div
@@ -76,8 +78,8 @@ class Slider extends React.Component {
                                 height: "20px",
                                 backgroundColor: "white",
                                 position: "relative",
-                                left: "98%",
-                                transform: "translateY(-7px)",
+                                left: "100%",
+                                transform: "translate(-10px,-7px)",
                                 cursor: "pointer"
                             }}
                             onMouseDown={this.sliderDragStart}
@@ -89,11 +91,14 @@ class Slider extends React.Component {
                         width: "100%",
                         textAlign: "justify",
                         padding: "0",
-                        
+
                     }}
                 >
-                    1620 1630 1640 1650 1660 1670 1680 1690 1700 1710 1720
-                    <span> </span>
+                    {this.stringifyDates()}
+                    <span style={{
+                        width: "100%",
+                        display: "inline-block"
+                    }}> </span>
                 </div>
             </div>
         );
