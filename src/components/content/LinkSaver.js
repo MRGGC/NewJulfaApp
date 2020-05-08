200098b8c487d8aeb500833bcf6755e8dad1c3e2
GGC_ <ggc.language@gmail.com>
2020-05-08 22:24:45 +0400

    get save content api

--- src/components/content/LinkSaver.js
+++ src/components/content/LinkSaver.js
@@ -1,14 +1,31 @@
-import React  from 'react';
+import React, { useContext } from 'react';
 import { IconContext } from 'react-icons';
 import { FaSave } from 'react-icons/fa';
+import { ContentContext } from '../../contexts/ContentContext';
+import { SaveContent } from '../../services/api';
 
 export default function LinkSaver() {
-    const handleSave = () => {
+    const contentContext = useContext(ContentContext);
+    const { current, list } = contentContext.articles;
 
+    const handleSave = async () => {
+        console.log("SAVING");
+        console.log(current, list);
+
+        let content = null;
+        for (let _content of list)
+            if (_content.id === current) content = _content;
+
+        const status = await SaveContent(content);
+
+        if (!status)
+            alert('ERROR');
+        else
+            alert("SAVED!");
     };
 
     return (
-        <div onClick={handleSave} className={"linkPart linkAdder"}>
+        <div onClick={() => handleSave()} className={"linkPart linkAdder"}>
             <IconContext.Provider value={{
                 className: "addIcon"
             }}>
