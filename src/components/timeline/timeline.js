ef94c184371e6b43ef696dfc24d1f3fdb0322fe6
Davit Grigoryan <ggc.language@gmail.com>
2020-07-03 13:02:16 +0400

    b4 backup

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -22,7 +22,7 @@ class Timeline extends React.Component {
         return (
             <>
                 <Map year={this.state.year} ref={this.map} />
-                <Slider values={[1620, 1630, 1640, 1650, 1660, 1670, 1680, 1690, 1700, 1710, 1720]} onYearChange={this.oyc} />
+                <Slider values={[1600, 1620, 1630, 1640, 1650, 1660, 1670, 1680, 1690, 1700, 1710, 1720, 1740]} onYearChange={this.oyc} />
             </>
         );
     }
--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
-export const SERVER_PROTOCOL  = 'https';
-export const SERVER_HOSTNAME  = 'new-julfa-api.herokuapp.com';
-export const SERVER_PORT      =  undefined;
+// export const SERVER_PROTOCOL  = 'https';
+// export const SERVER_HOSTNAME  = 'new-julfa-api.herokuapp.com';
+// export const SERVER_PORT      =  undefined;
+// export const TOKEN_TYPE       = 'Bearer';
+export const SERVER_PROTOCOL  = 'http';
+export const SERVER_HOSTNAME  = 'localhost';
+export const SERVER_PORT      =  3000;
 export const TOKEN_TYPE       = 'Bearer';
